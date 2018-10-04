9152f2f1e5b31a55bcacd1209d44733d85d34ca8
Manuel Velasco <manuel.velascosuarez@outlook.com>
2018-10-04 20:48:19 +0200

    Created a general not definitive structure of the Main class

--- src/hja/Main.java
+++ src/hja/Main.java
@@ -1,33 +1,37 @@
 package hja;
 
+import hja.Card.Card;
 import hja.Hand.Hand;
 import hja.Parser.CardListParser;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 	
-	public static void main(String... args){
+	public static void main(String... args) {
 		int option = Integer.parseInt(args[0]);
 		String inputFile = args[1];
 		String outputFile = args[2];
-		
 	}
 	
 	private void calculateBests(String inputFile, String outputFile) {
-		try{
-			Scanner scanner = new Scanner(new FileReader(inputFile));
+		try {
+			FileReader fileReader = new FileReader(inputFile);
 			
-			ArrayList<Card> cards = CardListParser.parseListCard(scanner);
+			ArrayList<Card> cards = CardListParser.parseListCard(fileReader, 5);
 			Hand hand = Algorithm.calculateBestHand(cards);
 			ArrayList<Hand> possibleHands = Algorithm.calculateBestPossibleHands(cards);
 			// print hand and possibleHands
 		}
 		catch (FileNotFoundException e) {
 			System.out.println("Fichero no encontrado: " + inputFile);
+		}
+		catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
 }
