97cd2cdecc8744bf27de1699a770d40bb6d6a12e
Manuel Velasco <manuel.velascosuarez@outlook.com>
2018-10-04 20:40:15 +0200

    Created and implemented Card package. It contains all the items needed to represent a Card. This package also provides a Factory, that handles the creation of cards.

---
+++ src/hja/Card/Card.java
@@ -0,0 +1,16 @@
+package hja.Card;
+
+public class Card implements Comparable<Card> {
+	public final Rank rank;
+	public final Suit suit;
+	
+	public Card(Rank rank, Suit suit) {
+		this.rank = rank;
+		this.suit = suit;
+	}
+	
+	@Override
+	public int compareTo(Card o) {
+		return o.rank.compareTo(this.rank);
+	}
+}
---
+++ src/hja/Card/CardFactory.java
@@ -0,0 +1,91 @@
+package hja.Card;
+
+public final class CardFactory {
+	private static Card[][] cards;
+	private static Suit[] suits;
+	private static Rank[] ranks;
+	
+	static {
+		CardFactory.suits = Suit.values();
+		CardFactory.ranks = Rank.values();
+		
+		CardFactory.cards = new Card[suits.length][ranks.length];
+		for (int i = 0; i < suits.length; ++i) {
+			for (int j = 0; j < ranks.length; ++j) {
+				CardFactory.cards[i][j] = new Card(ranks[j], suits[i]);
+			}
+		}
+	}
+	
+	public static Card createCard(char rank, char suit) {
+		Rank rRank = CardFactory.getRank(rank);
+		Suit sSuit = CardFactory.getSuit(suit);
+		
+		return createCard(rRank, sSuit);
+	}
+	
+	public static Card createCard(Rank rank, Suit suit) {
+		int suit_pos = indexOf(suit, suits);
+		int rank_pos = indexOf(rank, ranks);
+		
+		return CardFactory.cards[suit_pos][rank_pos];
+	}
+	
+	private static Suit getSuit(char suit) {
+		switch (suit) {
+			case 'h':
+				return Suit.HEARTS;
+			case 'd':
+				return Suit.DIAMONDS;
+			case 's':
+				return Suit.SPADES;
+			case 'c':
+				return Suit.CLUBS;
+			default:
+				return null;
+		}
+		
+	}
+	
+	private static Rank getRank(char rank) {
+		switch (rank) {
+			case '2':
+				return Rank.TWO;
+			case '3':
+				return Rank.THREE;
+			case '4':
+				return Rank.FOUR;
+			case '5':
+				return Rank.FIVE;
+			case '6':
+				return Rank.SIX;
+			case '7':
+				return Rank.SEVEN;
+			case '8':
+				return Rank.EIGHT;
+			case '9':
+				return Rank.NINE;
+			case 'T':
+				return Rank.TEN;
+			case 'J':
+				return Rank.JACK;
+			case 'Q':
+				return Rank.QUEEN;
+			case 'K':
+				return Rank.KING;
+			case 'A':
+				return Rank.ACE;
+			default:
+				return null;
+		}
+	}
+	
+	private static <T> int indexOf(T elem, T[] arr) {
+		int i = 0;
+		while (i < arr.length && elem != arr[i]) {
+			++i;
+		}
+		
+		return i;
+	}
+}
