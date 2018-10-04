178bd919c2ed2d4db91ea18c861505187454b24c
Manuel Velasco <manuel.velascosuarez@outlook.com>
2018-10-04 20:46:11 +0200

    Implemented a card parser

--- src/hja/Parser/CardListParser.java
+++ src/hja/Parser/CardListParser.java
@@ -1,13 +1,29 @@
 package hja.Parser;
 
-import hja.Card;
+import hja.Card.Card;
+import hja.Card.CardFactory;
 
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public final class CardListParser {
-
-	public static ArrayList<Card> parseListCard(Scanner stream){
-		return null;
+	
+	public static ArrayList<Card> parseListCard(InputStreamReader stream, int numCards) throws IOException {
+		ArrayList<Card> arr = new ArrayList<>(numCards);
+		
+		for(int i = 0; i < numCards; ++i){
+			Card card = parseCard(stream);
+			arr.set(i, card);
+		}
+		
+		return arr;
+	}
+	
+	private static Card parseCard(InputStreamReader stream) throws IOException {
+		char rank = (char) stream.read();
+		char suit = (char) stream.read();
+		
+		return CardFactory.createCard(rank, suit);
 	}
 }
