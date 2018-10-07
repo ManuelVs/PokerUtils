9406c29132a705f020a33decf2b683860e1c63d7
Manuel Velasco <manuel.velascosuarez@outlook.com>
2018-10-07 08:40:18 +0200

    Tweaked test cases

--- test/hja/Hand/HandTest.java
+++ test/hja/Hand/HandTest.java
@@ -35,29 +35,29 @@ class HandTest {
 	private static final Card nineh = new Card(Rank.NINE, Suit.HEARTS);
 	private static final Card eighth = new Card(Rank.EIGHT, Suit.DIAMONDS);
 	
-	private static final Hand hihgcard1 = new HighCard(new ArrayList<>(Arrays.asList(ace, king, seven, three, two )));
+	private static final Hand hihgcard1 = new HighCard(new ArrayList<>(Arrays.asList(ace, king, seven, three, two)));
 	private static final Hand hihgcard2 = new HighCard(new ArrayList<>(Arrays.asList(king, queen, seven, three, two)));
 	
-	private static final Hand pair1 = new Pair(new ArrayList<>(Arrays.asList(ace, ace, seven, three, two )));
-	private static final Hand pair2 = new Pair(new ArrayList<>(Arrays.asList(king, king, seven, three, two)));
+	private static final Hand pair1 = new Pair(new ArrayList<>(Arrays.asList(ace, ace, seven, three, two)));
+	private static final Hand pair2 = new Pair(new ArrayList<>(Arrays.asList(ace, king, king, seven, three)));
 	
-	private static final Hand twopair1 = new TwoPair(new ArrayList<>(Arrays.asList(ace, ace, seven, seven, two )));
-	private static final Hand twopair2 = new TwoPair(new ArrayList<>(Arrays.asList(king, king, three, three, two)));
+	private static final Hand twopair1 = new TwoPair(new ArrayList<>(Arrays.asList(ace, ace, seven, seven, two)));
+	private static final Hand twopair2 = new TwoPair(new ArrayList<>(Arrays.asList(ace, king, king, three, three)));
 	
-	private static final Hand threeof1 = new ThreeOfAKind(new ArrayList<>(Arrays.asList(ace, ace, ace, three, two )));
-	private static final Hand threeof2 = new ThreeOfAKind(new ArrayList<>(Arrays.asList(king, king, king, three, two)));
+	private static final Hand threeof1 = new ThreeOfAKind(new ArrayList<>(Arrays.asList(ace, ace, ace, three, two)));
+	private static final Hand threeof2 = new ThreeOfAKind(new ArrayList<>(Arrays.asList(ace, king, king, king, three)));
 	
-	private static final Hand straight1 = new Straight(new ArrayList<>(Arrays.asList(ace, king, queen, jack, ten )));
+	private static final Hand straight1 = new Straight(new ArrayList<>(Arrays.asList(ace, king, queen, jack, ten)));
 	private static final Hand straight2 = new Straight(new ArrayList<>(Arrays.asList(king, queen, jack, ten, nine)));
 	
 	private static final Hand flush1 = new Flush(new ArrayList<>(Arrays.asList(ace, ten, nine, four, two)));
 	private static final Hand flush2 = new Flush(new ArrayList<>(Arrays.asList(jack, eight, seven, five, three)));
 	
 	private static final Hand full1 = new FullHouse(new ArrayList<>(Arrays.asList(ace, ace, ace, five, five)));
-	private static final Hand full2 = new FullHouse(new ArrayList<>(Arrays.asList(queen, queen, queen, ten, ten)));
+	private static final Hand full2 = new FullHouse(new ArrayList<>(Arrays.asList(queen, queen, ten, ten, ten)));
 	
 	private static final Hand fourof1 = new FourOfAKind(new ArrayList<>(Arrays.asList(ace, ace, ace, ace, five)));
-	private static final Hand fourof2 = new FourOfAKind(new ArrayList<>(Arrays.asList(six, six, six, six, ten)));
+	private static final Hand fourof2 = new FourOfAKind(new ArrayList<>(Arrays.asList(ten, six, six, six, six)));
 	
 	private static final Hand straightf1 = new StraightFlush(new ArrayList<>(Arrays.asList(kingh, queenh, jackh, tenh, nineh)));
 	private static final Hand straightf2 = new StraightFlush(new ArrayList<>(Arrays.asList(queenh, jackh, tenh, nineh, eighth)));
