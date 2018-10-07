44fe0ce1c8c48653e2ab5e8318d24934ddc66e05
Manuel Velasco <manuel.velascosuarez@outlook.com>
2018-10-07 08:38:42 +0200

    Fixed a bug. Initial capacity parameter of ArrayList<> constructor does not work intuitively

--- src/hja/Parser/CardListParser.java
+++ src/hja/Parser/CardListParser.java
@@ -4,25 +4,25 @@ import hja.Card.Card;
 import hja.Card.CardFactory;
 
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.Reader;
 import java.util.ArrayList;
 
 public final class CardListParser {
 	
-	public static ArrayList<Card> parseListCard(InputStreamReader stream, int numCards) throws IOException {
+	public static ArrayList<Card> parseListCard(Reader reader, int numCards) throws IOException {
 		ArrayList<Card> arr = new ArrayList<>(numCards);
 		
 		for (int i = 0; i < numCards; ++i) {
-			Card card = parseCard(stream);
-			arr.set(i, card);
+			Card card = parseCard(reader);
+			arr.add(card);
 		}
 		
 		return arr;
 	}
 	
-	private static Card parseCard(InputStreamReader stream) throws IOException {
-		char rank = (char) stream.read();
-		char suit = (char) stream.read();
+	private static Card parseCard(Reader reader) throws IOException {
+		char rank = (char) reader.read();
+		char suit = (char) reader.read();
 		
 		return CardFactory.createCard(rank, suit);
 	}
