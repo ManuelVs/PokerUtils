bf7207f4d61d616c62e5abeb20377fac34bd33fb
Manuel Velasco <manuel.velascosuarez@outlook.com>
2018-10-07 08:36:53 +0200

    Implemented equals() method on Card

--- src/hja/Card/Card.java
+++ src/hja/Card/Card.java
@@ -13,4 +13,14 @@ public class Card implements Comparable<Card> {
 	public int compareTo(Card o) {
 		return this.rank.compareTo(o.rank);
 	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if(obj == null) return false;
+		if(! (obj instanceof Card)) return false;
+		if(this == obj) return true;
+		
+		Card other = (Card) obj;
+		return other.rank == this.rank && other.suit == this.suit;
+	}
 }
