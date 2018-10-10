b71e660103188a5e0167c25290c0a81c49650648
Manuel Velasco <manuel.velascosuarez@outlook.com>
2018-10-10 17:55:14 +0200

    Created main schema

--- src/hja/Main.java
+++ src/hja/Main.java
@@ -4,9 +4,7 @@ import hja.Card.Card;
 import hja.Hand.Hand;
 import hja.Parser.CardListParser;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 
 public class Main {
@@ -15,22 +13,76 @@ public class Main {
 		int option = Integer.parseInt(args[0]);
 		String inputFile = args[1];
 		String outputFile = args[2];
+		
+		try{
+			FileReader reader = new FileReader(inputFile);
+			FileWriter writer = new FileWriter(outputFile);
+			
+			switch (option){
+				case 1: classifyHand(reader, writer); break;
+				case 2: calculateBestHand(reader, writer); break;
+				case 3: sortPlayerHands(reader, writer); break;
+				case 4: calculateBestHandOmaha(reader, writer); break;
+			}
+			
+			reader.close();
+			writer.close();
+		}
+		catch (Exception e){
+			e.printStackTrace();
+		}
 	}
 	
-	private void calculateBests(String inputFile, String outputFile) {
-		try {
-			FileReader fileReader = new FileReader(inputFile);
+	private static void classifyHand(Reader input, Writer output) throws IOException {
+		while (input.ready()){
+			ArrayList<Card> cards = CardListParser.parseListCard(input, 5);
+			consumeLine(input);
 			
-			ArrayList<Card> cards = CardListParser.parseListCard(fileReader, 5);
-			Hand hand = Algorithm.calculateBestHand(cards);
-			ArrayList<Hand> possibleHands = Algorithm.calculateBestPossibleHands(cards);
-			// print hand and possibleHands
+			Hand bestHand = Algorithm.calculateHand(cards);
+			ArrayList<Hand> draws = Algorithm.calculateDraws(cards);
+			
+			output.write(cardsToString(cards));
+			output.write(System.lineSeparator());
+			output.write(" - Best hand: ");
+			output.write(bestHand.toString());
+			output.write(System.lineSeparator());
+			
+			for (Hand draw : draws) {
+				output.write(" - Draw: ");
+				output.write(draw.toString());
+				output.write(System.lineSeparator());
+			}
+			output.write(System.lineSeparator());
 		}
-		catch (FileNotFoundException e) {
-			System.out.println("Fichero no encontrado: " + inputFile);
+	}
+	
+	private static void calculateBestHand(Reader input, Writer output) {
+	
+	}
+	
+	private static void sortPlayerHands(Reader input, Writer output) {
+	
+	}
+	
+	private static void calculateBestHandOmaha(Reader input, Writer output) {
+	
+	}
+	
+	private static String cardsToString(ArrayList<Card> cards){
+		StringBuilder sb = new StringBuilder();
+		
+		for(Card card : cards){
+			sb.append(card);
 		}
-		catch (IOException e) {
-			e.printStackTrace();
+		
+		return sb.toString();
+	}
+	
+	private static void consumeLine(Reader reader) throws IOException {
+		char c = (char) reader.read();
+		
+		while (reader.ready() && c != '\n'){
+			c = (char) reader.read();
 		}
 	}
 }
