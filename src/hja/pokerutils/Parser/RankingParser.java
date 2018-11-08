d37b37c528acaeb04ba88ed5892ba1c1b466f367
Manuel Pastor Cobo <manupast@ucm.es>
2018-11-08 15:02:15 +0100

    Implemented class to parse rankings

---
+++ src/hja/pokerutils/Parser/RankingParser.java
@@ -0,0 +1,32 @@
+package hja.pokerutils.Parser;
+
+import hja.pokerutils.Card.CardFactory;
+import hja.pokerutils.Range.CardPair;
+import hja.pokerutils.Ranking.Ranking;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.util.ArrayList;
+
+public final class RankingParser {
+	public static Ranking parseRanking(Reader reader, int length)  throws IOException {
+		char[] cRanking = new char[length];
+		reader.read(cRanking);
+		
+		String ranking = new String(cRanking);
+		String[] parts = ranking.split(",");
+		
+		ArrayList<CardPair> cardPairArray = new ArrayList<>();
+		for (String p: parts) {
+			boolean isSuited = false;
+			if(p.length() == 3 && p.charAt(2) == 's'){
+				isSuited = true;
+			}
+			CardPair cp = new CardPair(CardFactory.getRank(p.charAt(0)), CardFactory.getRank(p.charAt(1)), isSuited);
+			
+			cardPairArray.add(cp);
+		}
+		
+		return new Ranking(cardPairArray);
+	}
+}
