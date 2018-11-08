2eed3fb1374f9a3f002aa71ee8cfafa4a5468eb0
Manuel Pastor Cobo <manupast@ucm.es>
2018-11-08 22:11:25 +0100

    Added listeners and new features to PercentageRange

--- src/hja/logic/gui/components/PercentageRange.java
+++ src/hja/logic/gui/components/PercentageRange.java
@@ -1,18 +1,30 @@
 package hja.logic.gui.components;
 
 import hja.logic.gui.model.Model;
+import hja.logic.gui.model.PercentageListener;
+import hja.logic.gui.model.RankingListener;
+import hja.pokerutils.Ranking.Ranking;
 
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class PercentageRange extends JPanel {
-	
+public class PercentageRange extends JPanel implements PercentageListener, RankingListener {
+	private Model model;
 	public PercentageRange(Model model){
+		this.model = model;
+		this.model.addRankingListener(this);
+		this.model.addPercentageListener(this);
 		initGUI();
 	}
 	
 	private void initGUI() {
 		JSlider slider = new JSlider(0, 100 * 100, JSlider.HORIZONTAL);
 		JLabel label = new JLabel("0.0");
+		label.setPreferredSize(new Dimension(40, 5));
 		
 		slider.addChangeListener((e) -> {
 			double val = slider.getValue();
@@ -24,5 +36,15 @@ public class PercentageRange extends JPanel {
 		this.add(label);
 		
 		slider.setValue(0);
+	}
+	
+	@Override
+	public void notify(double percentage) {
+	
+	}
+	
+	@Override
+	public void notify(Ranking ranking) {
+	
 	}
 }
