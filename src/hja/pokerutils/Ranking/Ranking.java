6d1c113c9953d5d75bab43ace69fb41d96893bf8
Manuel Pastor Cobo <manupast@ucm.es>
2018-11-08 22:13:12 +0100

    Modified Ranking class to calculate correctly the number of cards to select

--- src/hja/pokerutils/Ranking/Ranking.java
+++ src/hja/pokerutils/Ranking/Ranking.java
@@ -8,14 +8,15 @@ import java.util.ArrayList;
 public class Ranking {
 	private final ArrayList<CardPair> cardPairArray;
 	
+	public Ranking(){ this.cardPairArray = new ArrayList<>(); }
 	public Ranking(ArrayList<CardPair> cardPairArray){
 		this.cardPairArray = cardPairArray;
 	}
 	
 	public Range selectTopCardPairs(double percentage){
-		double numCards = 0;
+		long numCards = 0;
 		if(percentage > 0) {
-			numCards = this.cardPairArray.size() * percentage;
+			numCards = Math.round(this.cardPairArray.size() * percentage);
 		}
 		
 		Range out = new Range();
