3a9f4638dc68aade79c360814e04f7bb162c2f1f
Manuel Pastor Cobo <manupast@ucm.es>
2018-11-08 22:12:23 +0100

    Modified ModelImpl to have Ranking and Percentage listeners

--- src/hja/logic/gui/model/ModelImpl.java
+++ src/hja/logic/gui/model/ModelImpl.java
@@ -3,6 +3,7 @@ package hja.logic.gui.model;
 import hja.pokerutils.Algorithm.CombosAlgorithm;
 import hja.pokerutils.Card.Card;
 import hja.pokerutils.Range.Range;
+import hja.pokerutils.Ranking.Ranking;
 
 import javax.swing.*;
 import java.util.ArrayList;
@@ -11,19 +12,27 @@ public class ModelImpl implements Model {
 	private Range range;
 	private ArrayList<Card> cards;
 	private CombosAlgorithm.Combos combos;
+	private Ranking ranking;
+	private double percentage;
 	
 	private ArrayList<BoardCardsListener> boardCardsListeners;
 	private ArrayList<CombosListener> combosListeners;
 	private ArrayList<RangeListener> rangeListeners;
+	private ArrayList<RankingListener> rankingListeners;
+	private ArrayList<PercentageListener> percentageListeners;
 	
 	public ModelImpl(){
 		this.cards = new ArrayList<>(5);
 		this.range = new Range();
 		this.combos = new CombosAlgorithm.Combos();
+		this.ranking = new Ranking();
+		this.percentage = 0.0;
 		
 		this.boardCardsListeners = new ArrayList<>();
 		this.combosListeners = new ArrayList<>();
 		this.rangeListeners = new ArrayList<>();
+		this.rankingListeners = new ArrayList<>();
+		this.percentageListeners = new ArrayList<>();
 	}
 	
 	public void setRange(Range range) {
@@ -42,6 +51,20 @@ public class ModelImpl implements Model {
 		notifyBoardCardsListeners();
 	}
 	
+	public void setRanking(Ranking ranking){
+		this.ranking = ranking;
+		
+		updateCombos();
+		notifyRankingListener();
+	}
+	
+	public void setPercentage(double percentage){
+		this.percentage = percentage;
+		
+		updateCombos();
+		notifyPercentageListener();
+	}
+	
 	public void addBoardCardsListener(BoardCardsListener listener){
 		this.boardCardsListeners.add(listener);
 	}
@@ -52,6 +75,14 @@ public class ModelImpl implements Model {
 	
 	public void addRangeListener(RangeListener listener){
 		this.rangeListeners.add(listener);
+	}
+	
+	public void addRankingListener(RankingListener listener){
+		this.rankingListeners.add(listener);
+	}
+	
+	public void addPercentageListener(PercentageListener listener) {
+		this.percentageListeners.add(listener);
 	}
 	
 	private void updateCombos(){
@@ -85,6 +116,22 @@ public class ModelImpl implements Model {
 		SwingUtilities.invokeLater(() -> {
 			for(CombosListener listener : combosListeners){
 				listener.notify(combos);
+			}
+		});
+	}
+	
+	private void notifyRankingListener(){
+		SwingUtilities.invokeLater(() -> {
+			for(RankingListener listener : rankingListeners){
+				listener.notify(ModelImpl.this.ranking);
+			}
+		});
+	}
+	
+	private void notifyPercentageListener(){
+		SwingUtilities.invokeLater(() -> {
+			for(PercentageListener listener : percentageListeners){
+				listener.notify(ModelImpl.this.percentage);
 			}
 		});
 	}
