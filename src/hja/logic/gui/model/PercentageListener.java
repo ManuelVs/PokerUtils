4f59cfd6962b7260ad266776d521c73e53e20095
Manuel Pastor Cobo <manupast@ucm.es>
2018-11-09 01:19:47 +0100

    Percentage and Ranking listeners weren't really necessary, so I deleted them

--- src/hja/logic/gui/model/PercentageListener.java
+++
@@ -1,5 +0,0 @@
-package hja.logic.gui.model;
-
-public interface PercentageListener {
-	void notify(double percentage);
-}
--- src/hja/logic/gui/model/RankingListener.java
+++
@@ -1,7 +0,0 @@
-package hja.logic.gui.model;
-
-import hja.pokerutils.Ranking.Ranking;
-
-public interface RankingListener {
-	void notify(Ranking ranking);
-}
