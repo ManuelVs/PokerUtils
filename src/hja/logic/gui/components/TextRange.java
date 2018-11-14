9da45731878ddb3620eee275b47deb90a2489459
ccrp97 <44087436+ccrp97@users.noreply.github.com>
2018-11-14 13:54:40 +0100

    Update src/hja/logic/gui/components/TextRange.java

--- src/hja/logic/gui/components/TextRange.java
+++ src/hja/logic/gui/components/TextRange.java
@@ -1,18 +1,27 @@
 package hja.logic.gui.components;
 
 import hja.logic.gui.model.Model;
+import hja.logic.gui.model.RangeListener;
+import hja.pokerutils.Parser.RangeParser;
+import hja.pokerutils.Range.Range;
 
 import javax.swing.*;
 import java.awt.*;
 
-public class TextRange extends JPanel {
+public class TextRange extends JPanel implements RangeListener{
+	private Model model;
+	
+	private JTextField textField;
 	
 	public TextRange(Model model){
+		this.model = model;
 		initGUI();
+		
 	}
 	
 	private void initGUI() {
-		JTextField textField = new JTextField();
+		
+		this.textField = new JTextField();
 		JLabel label = new JLabel("Range: ");
 		label.setLabelFor(textField);
 		
@@ -24,5 +33,23 @@ public class TextRange extends JPanel {
 		this.add(textField);
 		this.add(button);
 		textField.setMaximumSize(new Dimension(Integer.MAX_VALUE, 100));
+		
+
+		
+		button.addActionListener((e) -> {
+			Range range = RangeParser.parseRange(textField.getText());
+			System.out.println(range.toString());
+			this.model.setRange(range);
+			});
+		
+		}
+		
+		
+		@Override
+		public void notify(Range range){
+			this.textField.setText(range.toString());
+		
+		}
+
+	
 	}
-}
