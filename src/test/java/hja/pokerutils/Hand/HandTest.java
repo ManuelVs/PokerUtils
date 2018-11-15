fa629921ee37768dba0ba573c80132ce96668109
Manuel Velasco <manuel.velascosuarez@outlook.com>
2018-11-15 22:48:25 +0100

    Changed location of source files

---
+++ src/test/java/hja/pokerutils/Hand/HandTest.java
@@ -0,0 +1,77 @@
+package hja.pokerutils.Hand;
+
+import hja.pokerutils.TestUtils;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class HandTest {
+	private static final Hand highCard1 = new HighCard(new ArrayList<>(Arrays.asList(TestUtils.ACEc, TestUtils.KINGd, TestUtils.SEVENc, TestUtils.THREEd, TestUtils.TWOd)));
+	private static final Hand highCard2 = new HighCard(new ArrayList<>(Arrays.asList(TestUtils.KINGd, TestUtils.QUEENd, TestUtils.SEVENc, TestUtils.THREEd, TestUtils.TWOh)));
+	
+	private static final Hand pair1 = new Pair(new ArrayList<>(Arrays.asList(TestUtils.ACEc, TestUtils.ACEd, TestUtils.SEVENc, TestUtils.THREEd, TestUtils.TWOs)));
+	private static final Hand pair2 = new Pair(new ArrayList<>(Arrays.asList(TestUtils.ACEc, TestUtils.KINGd, TestUtils.KINGh, TestUtils.SEVENs, TestUtils.THREEd)));
+	
+	private static final Hand twoPair1 = new TwoPair(new ArrayList<>(Arrays.asList(TestUtils.ACEc, TestUtils.ACEd, TestUtils.SEVENc, TestUtils.SEVENd, TestUtils.TWOc)));
+	private static final Hand twoPair2 = new TwoPair(new ArrayList<>(Arrays.asList(TestUtils.KINGd, TestUtils.KINGh, TestUtils.EIGHTs, TestUtils.THREEh, TestUtils.THREEc)));
+	
+	private static final Hand threeOf1 = new ThreeOfAKind(new ArrayList<>(Arrays.asList(TestUtils.ACEc, TestUtils.ACEd, TestUtils.ACEh, TestUtils.THREEs, TestUtils.TWOd)));
+	private static final Hand threeOf2 = new ThreeOfAKind(new ArrayList<>(Arrays.asList(TestUtils.ACEs, TestUtils.KINGd, TestUtils.KINGs, TestUtils.KINGc, TestUtils.FOURd)));
+	
+	private static final Hand straight1 = new Straight(new ArrayList<>(Arrays.asList(TestUtils.ACEc, TestUtils.KINGs, TestUtils.QUEENh, TestUtils.JACKh, TestUtils.TENd)));
+	private static final Hand straight2 = new Straight(new ArrayList<>(Arrays.asList(TestUtils.QUEENs, TestUtils.JACKd, TestUtils.TENh, TestUtils.NINEh, TestUtils.EIGHTd)));
+	
+	private static final Hand flush1 = new Flush(new ArrayList<>(Arrays.asList(TestUtils.ACEs, TestUtils.JACKs, TestUtils.TENs, TestUtils.NINEs, TestUtils.FOURs)));
+	private static final Hand flush2 = new Flush(new ArrayList<>(Arrays.asList(TestUtils.QUEENc, TestUtils.JACKc, TestUtils.TENc, TestUtils.FIVEc, TestUtils.FOURc)));
+	
+	private static final Hand full1 = new FullHouse(new ArrayList<>(Arrays.asList(TestUtils.ACEs, TestUtils.ACEd, TestUtils.ACEh, TestUtils.FIVEs, TestUtils.FIVEd)));
+	private static final Hand full2 = new FullHouse(new ArrayList<>(Arrays.asList(TestUtils.QUEENs, TestUtils.QUEENd, TestUtils.TENc, TestUtils.TENh, TestUtils.TENd)));
+	
+	private static final Hand fourOf1 = new FourOfAKind(new ArrayList<>(Arrays.asList(TestUtils.ACEs, TestUtils.ACEh, TestUtils.ACEd, TestUtils.ACEc, TestUtils.EIGHTc)));
+	private static final Hand fourOf2 = new FourOfAKind(new ArrayList<>(Arrays.asList(TestUtils.NINEd, TestUtils.SIXc, TestUtils.SIXd, TestUtils.SIXh, TestUtils.SIXs)));
+	
+	private static final Hand straightF1 = new StraightFlush(new ArrayList<>(Arrays.asList(TestUtils.KINGc, TestUtils.QUEENc, TestUtils.JACKc, TestUtils.TENc, TestUtils.NINEc)));
+	private static final Hand straightF2 = new StraightFlush(new ArrayList<>(Arrays.asList(TestUtils.EIGHTh, TestUtils.SEVENh, TestUtils.SIXh, TestUtils.FIVEh, TestUtils.FOURh)));
+	
+	private static final Hand royalF = new RoyalFlush(new ArrayList<>(Arrays.asList(TestUtils.ACEh, TestUtils.KINGh, TestUtils.QUEENh, TestUtils.JACKh, TestUtils.TENh)));
+	
+	private static Stream<Arguments> compareProvider() {
+		return Stream.of(
+				Arguments.of(highCard1, highCard2),
+				Arguments.of(pair1, pair2),
+				Arguments.of(twoPair1, twoPair2),
+				Arguments.of(threeOf1, threeOf2),
+				Arguments.of(straight1, straight2),
+				Arguments.of(flush1, flush2),
+				Arguments.of(full1, full2),
+				Arguments.of(fourOf1, fourOf2),
+				Arguments.of(straightF1, straightF2),
+				
+				Arguments.of(royalF, straightF1),
+				Arguments.of(straightF1, fourOf1),
+				Arguments.of(fourOf1, full2),
+				Arguments.of(full1, flush2),
+				Arguments.of(flush2, straight2),
+				Arguments.of(straight2, threeOf2),
+				Arguments.of(threeOf2, twoPair1),
+				Arguments.of(twoPair1, pair1),
+				Arguments.of(twoPair1, highCard2)
+		);
+	}
+	
+	@ParameterizedTest(name = "{index} => left={0}, right={1}")
+	@MethodSource("compareProvider")
+	void compareToTest(Hand left, Hand right) {
+		int compareGreater = left.compareTo(right);
+		assertTrue(compareGreater > 0);
+		
+		int compareLess = right.compareTo(left);
+		assertTrue(compareLess < 0);
+	}
+}
---
+++ src/test/java/hja/pokerutils/Parser/RangeParserTest.java
@@ -0,0 +1,118 @@
+package hja.pokerutils.Parser;
+
+import hja.pokerutils.Card.Rank;
+import hja.pokerutils.Range.CardPair;
+import hja.pokerutils.Range.Range;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RangeParserTest {
+	private static Stream<Arguments> rangeProvider() {
+		return Stream.of(
+				Arguments.of("AA", new ArrayList<>(Arrays.asList(
+						new CardPair(Rank.ACE, Rank.ACE, false))
+				)),
+				Arguments.of("AQs", new ArrayList<>(Arrays.asList(
+						new CardPair(Rank.ACE, Rank.QUEEN, true))
+				)),
+				Arguments.of("AQo", new ArrayList<>(Arrays.asList(
+						new CardPair(Rank.ACE, Rank.QUEEN, false))
+				)),
+				Arguments.of("94s", new ArrayList<>(Arrays.asList(
+						new CardPair(Rank.NINE, Rank.FOUR, true))
+				)),
+				Arguments.of("94s+", new ArrayList<>(Arrays.asList(
+						new CardPair(Rank.NINE, Rank.FOUR, true),
+						new CardPair(Rank.NINE, Rank.FIVE, true),
+						new CardPair(Rank.NINE, Rank.SIX, true),
+						new CardPair(Rank.NINE, Rank.SEVEN, true),
+						new CardPair(Rank.NINE, Rank.EIGHT, true))
+				)),
+				Arguments.of("55+", new ArrayList<>(Arrays.asList(
+						new CardPair(Rank.FIVE, Rank.FIVE, false),
+						new CardPair(Rank.SIX, Rank.SIX, false),
+						new CardPair(Rank.SEVEN, Rank.SEVEN, false),
+						new CardPair(Rank.EIGHT, Rank.EIGHT, false),
+						new CardPair(Rank.NINE, Rank.NINE, false),
+						new CardPair(Rank.TEN, Rank.TEN, false),
+						new CardPair(Rank.JACK, Rank.JACK, false),
+						new CardPair(Rank.QUEEN, Rank.QUEEN, false),
+						new CardPair(Rank.KING, Rank.KING, false),
+						new CardPair(Rank.ACE, Rank.ACE, false))
+				)),
+				Arguments.of("92s-95s", new ArrayList<>(Arrays.asList(
+						new CardPair(Rank.NINE, Rank.TWO, true),
+						new CardPair(Rank.NINE, Rank.THREE, true),
+						new CardPair(Rank.NINE, Rank.FOUR, true),
+						new CardPair(Rank.NINE, Rank.FIVE, true))
+				)),
+				Arguments.of("ATo-AKo", new ArrayList<>(Arrays.asList(
+						new CardPair(Rank.ACE, Rank.TEN, false),
+						new CardPair(Rank.ACE, Rank.JACK, false),
+						new CardPair(Rank.ACE, Rank.QUEEN, false),
+						new CardPair(Rank.ACE, Rank.KING, false))
+				)),
+				Arguments.of("ATs-A2s", new ArrayList<>(Arrays.asList(
+						new CardPair(Rank.ACE, Rank.TEN, true),
+						new CardPair(Rank.ACE, Rank.NINE, true),
+						new CardPair(Rank.ACE, Rank.EIGHT, true),
+						new CardPair(Rank.ACE, Rank.SEVEN, true),
+						new CardPair(Rank.ACE, Rank.SIX, true),
+						new CardPair(Rank.ACE, Rank.FIVE, true),
+						new CardPair(Rank.ACE, Rank.FOUR, true),
+						new CardPair(Rank.ACE, Rank.THREE, true),
+						new CardPair(Rank.ACE, Rank.TWO, true))
+				))
+		);
+	}
+	
+	private static Stream<Arguments> outOfRangeProvider() {
+		return Stream.of(
+				Arguments.of("ATo-AAo"),
+				Arguments.of("54s, ATo-AAo"),
+				Arguments.of("32s-3As")
+		);
+	}
+	
+	private static Stream<Arguments> malformedProvider() {
+		return Stream.of(
+				Arguments.of("AAs"),
+				Arguments.of("AAo"),
+				Arguments.of("QQs"),
+				Arguments.of("93s,65o,JJo")
+		);
+	}
+	
+	@ParameterizedTest(name = "{index} => {0}")
+	@MethodSource("rangeProvider")
+	void parseListCardTest(String sRange, ArrayList<CardPair> aRange) {
+		Range range = RangeParser.parseRange(sRange);
+		
+		int i = 0;
+		for (CardPair cp : range) {
+			assertTrue(aRange.contains(cp));
+			++i;
+		}
+		
+		assertEquals(aRange.size(), i);
+	}
+	
+	@ParameterizedTest(name = "{index} => {0}")
+	@MethodSource("outOfRangeProvider")
+	void testOutOfBoundsRange(String range) {
+		assertThrows(IllegalArgumentException.class, () -> RangeParser.parseRange(range));
+	}
+	
+	@ParameterizedTest(name = "{index} => {0}")
+	@MethodSource("malformedProvider")
+	void testMalformedRange(String range) {
+		assertNull(RangeParser.parseRange(range));
+	}
+}
